84cce99b8ac187bde669ee2a3465038c91d0510a
noumi553 <iamtayyabkhan568@gmail.com>
2024-08-10 23:57:25 -0700

    added index.html

---
+++ converter.js
@@ -0,0 +1,66 @@
+let country_code={
+    "NPR":"NP",
+    "BRL":"BR",
+    "INR":"IN",
+    "AUD":"AU",
+    "AED":"AE",
+    "AFN":"AF",
+    "XCD":"AG",
+    "ARS":"AR",
+    "BAM":"BA",
+    "BBD":"BB",
+    "BOB":"BO",
+}
+
+let droplist=document.querySelectorAll(".drop-list select");
+let btn=document.querySelector("form button");
+fromcurrency=document.querySelector(".from select");
+tocurrency=document.querySelector(".to select");
+
+
+for(select of droplist){
+    for(currency_code in country_code){
+        let newoption=document.createElement("option");
+        newoption.innerText=currency_code;
+        newoption.value=currency_code;
+        if(select.name==="from"&&currency_code==="AED"){
+            newoption.selected="selected";
+        }else if(select.name==="to"&&currency_code==="NPR"){
+            newoption.selected="selected";
+        };
+        select.append(newoption);
+    };
+    select.addEventListener("change",(e)=>{
+        loadflag(e.target);
+    });
+};
+
+function loadflag(e){
+  for(currency_code in country_code){
+    if(currency_code===e.value){
+        let newimg=e.parentElement.querySelector("img");
+        newimg.src=`https://flagsapi.com/${country_code[currency_code]}/flat/64.png`;
+    }
+  }
+}
+
+btn.addEventListener('click',(e)=>{
+e.preventDefault();
+getexchangerate();
+});
+
+function getexchangerate(){
+    let amount =document.querySelector(".amount input");
+    displayexchangerate=document.querySelector(".msg");
+    let amountval=amount.value;
+    if(amountval==="" || amountval>"0"){
+           amount.value="1"
+    }
+    displayexchangerate.innerText="Getting exchange..."
+    let url=`https://v6.exchangerate-api.com/v6/11bca9d2d09fd28b7a0df404/latest/${fromcurrency.value}`;
+fetch(url).then(response=>response.json()).then(result=>{
+    let exchangerate=result.conversion_rates[tocurrency.value];
+    let totalexchangerate=(amountval*exchangerate).toFixed(2);
+    displayexchangerate.innerText=`${amountval} ${fromcurrency.value}=${totalexchangerate} ${tocurrency.value}`;
+});
+};
---
+++ gsap.js
@@ -0,0 +1,14 @@
+var tl=gsap.timeline();
+
+tl.from("#box",{
+    opacity: 0,
+    duration: 2,
+    delay: 0.5,
+    y:-90,
+ });
+
+ tl.from("#myp",{
+    Opacity: 0,
+    y:-20,
+    duration: 1,
+ });
